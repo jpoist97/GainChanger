6b7c8fa1011d209e39a67a041d1b44b18ea67d6f
jpoist97 <jpoist97@gmail.com>
2020-10-15 11:43:08 -0700

    Upload demo for react-native-paper

--- components/workouts/Workouts.jsx
+++ components/workouts/Workouts.jsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
+import { Button } from 'react-native-paper';
 
 export default (props) => {
    return (    
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
          <Text>Workout Screen</Text>
          <Text>Replace these Text tags with your component</Text>
+         <Button contentStyle={{width: 300, height: 150,}} style={[{backgroundColor: "#5DB075", borderRadius: 15, borderWidth: 3, borderColor: "#5DB075", position: "absolute", top: 75, alignSelf: "center"}]} icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+            Press me
+         </Button>
       </View>);
 }
