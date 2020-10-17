58afc3267a2c1947fa1652f6bc9da85985e2689e
rohithdara <rohith.dara2000@gmail.com>
2020-10-16 23:28:26 -0700

    Added variable # of workout + buttons

--- components/home/Home.jsx
+++ components/home/Home.jsx
@@ -1,112 +1,85 @@
 import * as React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Button, Title, Subheading} from 'react-native-paper';
+import { View, StyleSheet } from 'react-native';
+import { Button, Title } from 'react-native-paper';
+import WorkoutContainer from './WorkoutContainer';
 
-{/* Temporary workout variables*/}
-let workout1 = "Workout 1"
-let workout2 = "Workout 2"
-const workoutList = [{name: 'workout name'}, {name: 'workout name'}]
+export default () => (
+  <View style={styles.container}>
 
+    {/* Cycles Section */}
+    <View style={{ ...styles.sectionComponent, margin: 30 }}>
 
+      <Title style={styles.title}>
+        Cycles
+      </Title>
 
-export default class App extends React.Component {
+      <Button contentStyle={{ width: 331, height: 170 }} style={styles.startCycle} color="#FFFFFF" onPress={() => alert('This should start the current workout in the cycle')}>
+        Start Cycle Workout
+      </Button>
 
-   render() {
-      return (
-         <View style = {styles.container}>
+      <Button contentStyle={{ width: 325, height: 35 }} style={styles.seeAllButton} color="#000000" onPress={() => alert('This should take you to workouts tab')}>
+        See All Cycles
+      </Button>
 
+    </View>
 
-            {/*Cycles Section*/}
-            <View style = {{...styles.sectionComponent, margin: 30}}>
+    {/* Workouts Section */}
+    <View style={{ ...styles.sectionComponent, margin: 10 }}>
+      <Title style={styles.title}>
+        Workouts
+      </Title>
 
-               <Title style = {styles.title}>
-                  Cycles
-               </Title>
+      <WorkoutContainer
+        items={[{ name: 'Push Workout A' }, { name: 'Pull Workout A' }]}
+      />
 
-               <Button contentStyle={{width: 331, height: 170}} style = {styles.startCycle} color = "#FFFFFF" onPress = {() => alert("This should start the current workout in the cycle")}>
-                  Start Cycle Workout
-               </Button>
+      <Button contentStyle={{ width: 325, height: 35 }} style={styles.seeAllButton} color="#000000" onPress={() => alert('This should take you to workouts tab')}>
+        See All Workouts
+      </Button>
 
-               <Button contentStyle = {{width: 325, height: 35}} style = {styles.seeAllButton} color = "#000000" onPress = {() => alert("This should take you to workouts tab")}>
-                  See all cycles
-               </Button>
-               
-            </View>
-
-
-            {/*Workouts Section*/}
-            <View style = {{...styles.sectionComponent, margin: 10}}>
-               <Title style = {styles.title}>
-                  Workouts
-               </Title>
-
-               <Subheading style = {[{position: "absolute", top: 82, left: 10, fontSize: 18}]}>
-                  Workout 1
-               </Subheading>
-
-               <Subheading style = {[{position: "absolute", top: 157, left: 10, fontSize: 18}]}>
-                  Workout 2
-               </Subheading>
-
-               <Button contentStyle = {{width: 100, height: 35}} style = {{...styles.startButton, right: 10, top: 75}} color = "#FFFFFF" onPress = {() => alert("This should start recording " + workout1)}>
-                  Start
-               </Button>
-
-               <Button contentStyle = {{width: 100, height: 35}} style = {{...styles.startButton, right: 10, top: 150}} color = "#FFFFFF" onPress = {() => alert("This should start recording " + workout2)}>
-                  Start
-               </Button>
-                  
-               <Button contentStyle = {{width: 325, height: 35}} style = {styles.seeAllButton} color = "#000000" onPress = {() => alert("This should take you to workouts tab")}>
-                  See all workouts
-               </Button>
-
-                 
-            </View>
-         </View>
-      )
-   }
-}
+    </View>
+  </View>
+);
 
 const styles = StyleSheet.create({
-   container: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-      backgroundColor: "#F6F6F6",
-   },
-   title: {
-      textAlign: "center",
-      marginVertical: 8,
-      fontSize: 28,
-   },
-   startButton: {
-      backgroundColor: "#5DB075",
-      borderRadius: 20,
-      position: "absolute"
-   },
-   seeAllButton: {
-      borderRadius: 20, 
-      borderWidth: 3, 
-      borderColor: "#5DB075", 
-      position: "absolute", 
-      bottom: 20, 
-      alignSelf: "center"
-   },
-   sectionComponent: {
-      width: "95%", 
-      height: "40%", 
-      backgroundColor: "#F6F6F6", 
-      borderWidth: 5, 
-      borderRadius: 20, 
-      borderColor: "#5DB075"
-   },
-   startCycle: {
-      backgroundColor: "#5DB075", 
-      borderRadius: 20, 
-      borderColor: "#5DB075", 
-      position: "absolute", 
-      top: 60, 
-      alignSelf: "center"
-   }
-})
-
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+    fontSize: 28,
+  },
+  startButton: {
+    backgroundColor: '#5DB075',
+    borderRadius: 20,
+    position: 'absolute',
+  },
+  seeAllButton: {
+    borderRadius: 20,
+    borderWidth: 3,
+    borderColor: '#5DB075',
+    position: 'absolute',
+    bottom: 20,
+    alignSelf: 'center',
+  },
+  sectionComponent: {
+    width: '95%',
+    height: '40%',
+    backgroundColor: '#F6F6F6',
+    borderWidth: 5,
+    borderRadius: 20,
+    borderColor: '#5DB075',
+  },
+  startCycle: {
+    backgroundColor: '#5DB075',
+    borderRadius: 20,
+    borderColor: '#5DB075',
+    position: 'absolute',
+    top: 60,
+    alignSelf: 'center',
+  },
+});
---
+++ components/home/WorkoutContainer.jsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Button, Subheading } from 'react-native-paper';
+
+const buttonStartLocation = 28;
+const textStartLocation = 30;
+
+export default (props) => (
+  <View>
+    {props.items.map((item, index) => (
+      <Button contentStyle={{ width: 100, height: 35 }} style={{ ...styles.startButton, top: buttonStartLocation + index * 70 }} color="#FFFFFF" onPress={() => alert(`This should start recording ${item.name}`)}>
+        Start
+      </Button>
+    ))}
+
+    {props.items.map((item, index) => (
+      <Subheading style={[{
+        position: 'absolute', top: textStartLocation + index * 70, left: 15, fontSize: 18,
+      }]}
+      >
+        {item.name}
+      </Subheading>
+    ))}
+  </View>
+
+);
+
+const styles = StyleSheet.create({
+  startButton: {
+    backgroundColor: '#5DB075',
+    borderRadius: 20,
+    position: 'absolute',
+    right: 15,
+  },
+});
