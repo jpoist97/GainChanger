138a23375e3156e90921799a883ef49af5eca205
jpoist97 <jpoist97@gmail.com>
2020-11-05 20:07:31 -0800

    Update Calendar.jsx

--- components/calendar/Calendar.jsx
+++ components/calendar/Calendar.jsx
@@ -1,46 +1,10 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
-import LogWorkout from '../workouts/LogWorkout';
 
 // eslint-disable-next-line no-unused-vars
 export default (props) => (
-  <LogWorkout
-    name="Pull A"
-    exercises={[{
-      name: 'Bicep Curls',
-      sets: [{
-        weight: '30', reps: '8',
-      }, {
-        weight: '30', reps: '8',
-      }, {
-        weight: '30', reps: '8',
-      }, {
-        weight: '30', reps: '8',
-      }],
-    }, {
-      name: 'Rows',
-      color: '#6D8DFF',
-      sets: [{
-        weight: '150', reps: '12',
-      }, {
-        weight: '150', reps: '12',
-      }, {
-        weight: '150', reps: '12',
-      }, {
-        weight: '150', reps: '12',
-      }],
-    }, {
-      name: 'Reverse Fly',
-      color: '#9D8DFF',
-      sets: [{
-        weight: '25', reps: '20',
-      }, {
-        weight: '25', reps: '20',
-      }, {
-        weight: '25', reps: '20',
-      }, {
-        weight: '25', reps: '20',
-      }],
-    }]}
-  />
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>Calendar Screen</Text>
+    <Text>Replace these Text tags with your component</Text>
+  </View>
 );
