1c380a4b4dba1df94d2a06ef533a2facc51c2253
jpoist97 <jpoist97@gmail.com>
2020-11-17 14:04:22 -0800

    Fix syntax

--- components/logWorkout/LogHeader.jsx
+++ components/logWorkout/LogHeader.jsx
@@ -26,7 +26,7 @@ const LogHeader = ({ type }) => (
 
     <StyledText style={{ width: 70 }}>lbs</StyledText>
 
-    <StyledText style={{ width: 70 }}>{type === 'REPS' ? 'Reps' : 'Secs'}</StyledText>}
+    <StyledText style={{ width: 70 }}>{type === 'REPS' ? 'Reps' : 'Secs'}</StyledText>
 
     <StyledText style={{ width: 40 }}>Done</StyledText>
   </Container>
