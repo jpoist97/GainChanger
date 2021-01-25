21f1f46d7286390a2ed43289132cd93b08f096f3
rohithdara <rohith.dara2000@gmail.com>
2021-01-24 21:29:36 -0800

    Added and styled calendar package

--- components/Calendar/Calendar.jsx
+++ components/Calendar/Calendar.jsx
@@ -1,10 +1,34 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import { Calendar } from 'react-native-calendars';
 
 // eslint-disable-next-line no-unused-vars
 export default (props) => (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text>Calendar Screen</Text>
-    <Text>Replace these Text tags with your component</Text>
+  <View style={{ flex: 1, justifyContent: 'center', height: 500}}>
+    <Calendar
+      // Specify style for calendar container element. Default = {}
+      style={{
+        height: 350,      
+      }}
+      // Specify theme properties to override specific styles for calendar parts. Default = {}
+      theme={{
+        backgroundColor: '#f2f2f2',
+        calendarBackground: '#f2f2f2',
+        todayTextColor: '#cab0ff',
+        dotColor: '#cab0ff',
+        arrowColor: '#cab0ff',
+        monthTextColor: '#cab0ff',
+        textDayFontFamily: 'Montserrat_500Medium',
+        textMonthFontFamily: 'Montserrat_600SemiBold',
+        textDayHeaderFontFamily: 'Montserrat_500Medium',
+        textDayFontSize: 14,
+      }}
+      markedDates={{
+        '2021-01-15': {marked: true},
+        '2021-01-16': {marked: true}
+      }}
+      onDayPress={(day) => {console.log('selected day', day["dateString"])}} //CHANGE TO SHOW RECORD COMPONENT
+      enableSwipeMonths={true}
+    />
   </View>
 );
