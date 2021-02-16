2bb7d4d57e768f66e5a64ed123f8a565e33d6b00
Cole <ctperry0301@gmail.com>
2021-02-16 15:07:47 -0800

    Create Custom Exercise Form (Frontend)

---
+++ components/AddExercises/CreateCustomExercise.jsx
@@ -0,0 +1,140 @@
+import React, { useState } from 'react';
+import {
+  TouchableOpacity, Button,
+} from 'react-native';
+import Collapsible from 'react-native-collapsible';
+import { AntDesign } from '@expo/vector-icons';
+import styled from 'styled-components/native';
+import RNPickerSelect from 'react-native-picker-select';
+import * as firebase from 'firebase';
+
+const Name = styled.Text`
+font-family: 'Montserrat_600SemiBold';
+font-size: 16px;
+margin-left: 8px;
+margin-bottom: 10px;
+padding: 4px;
+`;
+
+const IconButton = styled(AntDesign)`
+position: absolute;
+right: 10px;
+top: 0px;
+height: 25px;
+width: 25px;
+margin: 0px 25px 0px 0px;
+`;
+
+const Container = styled.View`
+flex-direction: row;
+align-items: center;
+justify-content: space-evenly; 
+height: 50px; 
+width: 100%;
+`;
+
+const WorkoutNameInput = styled.TextInput`
+font-size: 14px;
+borderBottomWidth: 1px;
+width: 40%;
+padding: 6px;
+`;
+
+const SelectWrapper = styled.View`
+font-family: 'Montserrat_600SemiBold';
+font-size: 24px;
+borderBottomWidth: 1px;
+width: 40%;
+padding: 6px;
+`;
+
+const CreateCustomExercise = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [workoutName, setWorkoutName] = React.useState('');
+  const [muscleGroup, setMuscleGroup] = React.useState('');
+
+  const createCustomExercise = async (exercise) => {
+    const currentUser = firebase.auth().currentUser.uid;
+    const customExerciseRef = firebase.firestore()
+      .collection('users')
+      .doc(currentUser)
+      .collection('customExercises');
+    await customExerciseRef.add(exercise);
+    setIsExpanded(false);
+    setMuscleGroup('');
+    setWorkoutName('');
+  };
+
+  return (
+    <TouchableOpacity onPress={() => setIsExpanded(!isExpanded)}>
+      <Name>Create a Custom Exercise</Name>
+      {isExpanded
+        ? (
+          <IconButton
+            name="up"
+            size={24}
+            color="#CAB0FF"
+          />
+        )
+        : (
+          <IconButton
+            name="down"
+            size={24}
+            color="#CAB0FF"
+          />
+        )}
+      <Collapsible collapsed={!isExpanded}>
+        <Container>
+          <WorkoutNameInput
+            onChangeText={(text) => setWorkoutName(text)}
+            value={workoutName}
+            placeholder="Workout Name"
+          />
+
+          <SelectWrapper>
+            <RNPickerSelect
+              onValueChange={(text) => setMuscleGroup(text)}
+              placeholder={{
+                label: 'Select a Muscle Group',
+                value: null,
+              }}
+              value={muscleGroup}
+              items={[
+                { label: 'Arms', value: 'Arms' },
+                { label: 'Back', value: 'Back' },
+                { label: 'Biceps', value: 'Biceps' },
+                { label: 'Calves', value: 'Calves' },
+                { label: 'Cardio', value: 'Cardio' },
+                { label: 'Chest', value: 'Chest' },
+                { label: 'Core', value: 'Core' },
+                { label: 'Full Body', value: 'Full Body' },
+                { label: 'Legs', value: 'Legs' },
+                { label: 'Olympic', value: 'Olympic' },
+                { label: 'Shoulders', value: 'Shoulders' },
+                { label: 'Triceps', value: 'Triceps' },
+              ]}
+            />
+          </SelectWrapper>
+        </Container>
+        <Button
+          title="Create Workout"
+          onPress={() => {
+            if (!workoutName) {
+              alert('Please enter a workout name');
+            } else if (!workoutName) {
+              alert('Please select a muscle group');
+            } else {
+              const exercise = {
+                name: workoutName,
+                muscleGroups: muscleGroup,
+              };
+              createCustomExercise(exercise);
+            }
+          }}
+        />
+      </Collapsible>
+    </TouchableOpacity>
+  );
+};
+
+export default CreateCustomExercise;
